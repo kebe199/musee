854a838a8caa161609104c22de81f7556fd08823
bvmbv <diagne.ahmadou-bamba@gmail.com>
2025-10-09 08:47:23 +0000

    changes

--- backend/importWorks.js
+++ backend/importWorks.js
@@ -1,11 +1,14 @@
-require('dotenv').config(); 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+// require('dotenv').config(); 
 
+
+const DATABASE_URL = 'postgresql://neondb_owner:npg_KQHZ2uwcl0Xo@ep-calm-heart-ad8iass8-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
 const { Pool } = require('pg');
 const fs = require('fs');
 
+console.log("DATABASE_URL =", DATABASE_URL);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // requis pour Neon
 });
 
